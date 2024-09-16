5dc8363b74ecc5cdb21e3fdf0531f44a0c7e829d
rafie <rafie.asadel@ui.ac.id>
2024-09-16 16:50:19 +0700

    fix: Wrong redirect name path

--- main/urls.py
+++ main/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from .views import create_product, view_all_product,show_xml, show_json, show_xml_by_id, show_json_by_id
 
 urlpatterns = [
-    path('', lambda request: redirect('view-all-product')),
-    path('create-product', create_product, name='create_product'),
+    path('', lambda request: redirect('view_all_product')),
     path('view-all-product', view_all_product, name='view_all_product'),
+    path('create-product', create_product, name='create_product'),
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'), 
     path('xml/<str:id>/', show_xml_by_id, name='show_xml_by_id'),
