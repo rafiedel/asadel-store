ac10b15052c794eac5ce4850cc8be9f6eeacc0a6
rafie <rafie.asadel@ui.ac.id>
2024-10-02 12:42:21 +0700

    fix: Add CSRF TRUSTED ORIGINS

--- asadelstore/settings.py
+++ asadelstore/settings.py
@@ -136,3 +136,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost", "http://127.0.0.1","http://rafie-asadel-mentalhealthtracker.pbp.cs.ui.ac.id/", "https://rafie-asadel-mentalhealthtracker.pbp.cs.ui.ac.id/"]
